5e93f1b92d24d2d1fa6964ad50eb5a215345d595
Lester Hurtado <lester@hashtagdigital.com>
2017-05-25 23:50:35 +0800

    Change Laravel to Optimax 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Optimax
+                    Hashtag Digital Optimax
                 </div>
 
                 <div class="links">
